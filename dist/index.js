837d0117db2769161810440bed31cd0516792cac
Tommy Fetterleigh <tfetterleigh@mosssavings.com>
2025-10-22 10:36:38 -0700

    refactor: GitHub Action to simplify event handling logic
    - Added `workflow_dispatch` trigger for manual workflow runs in `greet.yml`.
    - Simplified `src/index.ts` to streamline PR and workflow-related event handling.
    - Removed unused inputs (`label`) and dependencies (`@actions/core`, etc.).
    - Updated compiled `dist/` files to reflect the adjusted logic.

--- dist/index.js
+++ dist/index.js
@@ -43,22 +43,19 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.run = run;
 const github = __importStar(__nccwpck_require__(3228));
 async function run() {
-    const isPullRequest = (github.context.eventName === 'pull_request' || github.context.eventName === 'pull_request_target');
-    const prIsClosed = isPullRequest && github.context.payload.action === 'closed';
+    const isPullRequest = github.context.eventName === "pull_request" || github.context.eventName === "pull_request_target";
+    const prIsClosed = isPullRequest && github.context.payload.action === "closed";
     if (prIsClosed) {
-        log('is PR event and PR is closed');
+        console.log("gitflow-action: is PR event and PR is closed");
     }
-    else if (github.context.eventName === 'workflow_dispatch') {
-        log('is workflow_dispatch');
+    else if (github.context.eventName === "workflow_dispatch") {
+        console.log("gitflow-action: is workflow_dispatch");
     }
     else {
-        log('no conditions matched');
+        console.log("gitflow-action: no conditions matched");
     }
 }
-function log(message) {
-    const logPrefix = 'gitflow-action:';
-    console.log(`${logPrefix} ${message}`);
-}
+/* istanbul ignore next */
 if (!process.env.JEST_WORKER_ID) {
     run();
 }
